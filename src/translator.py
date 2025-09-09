6f3e78841cb7fa691c21a80dc8be50284998e674
Serge46 <serg46b@gmail.com>
2025-09-09 16:51:33 +0700

    Added inline comments and requirements

--- src/translator.py
+++ src/translator.py
@@ -4,9 +4,27 @@ pipe_ru2en = pipeline("translation", model="Helsinki-NLP/opus-mt-ru-en")
 
 
 def translate_en(en: str) -> str:
+    """
+    Translates English text to Russian.
+
+    Args:
+        en (str): The English text to translate.
+
+    Returns:
+        str: The translated Russian text.
+    """
     return pipe_en2ru(en)[0]['translation_text']
 
 def translate_ru(ru: str) -> str:
+    """
+    Translates Russian text to English.
+
+    Args:
+        ru (str): The Russian text to translate.
+
+    Returns:
+        str: The translated English text.
+    """
     return pipe_ru2en(ru)[0]['translation_text']
 
 
